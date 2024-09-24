6bfb18610bb3df52786b58212b4f4ab9a0f5740b
Your Name <you@example.com>
2024-09-24 19:37:38 +0300

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,68 @@
+var form=document.querySelector(".form-container");
+var nameInput=document.querySelector(".name");
+var priceInput=document.querySelector(".price");
+var descriptionInput=document.querySelector(".description");
+var cancleBtn=document.querySelector(".cancel-btn");
+var tableBody=document.querySelector(".table-body");
+
+
+
+var products=[];
+var tempProduct={};
+
+
+function reloadTable(){
+if(products.length==0){
+    tableBody.innerHTML=`<h2 style="background-color:red">No element</h2>`;
+}
+else{
+    
+    var content="";
+    for(var i=0;i<products.length;i++){
+        content+=
+        `<tr>
+            <td>${products[i].name}</td>
+            <td>${products[i].price}</td>
+            <td>${products[i].description}</td>
+</tr>`;
+    }
+    
+    tableBody.innerHTML=content;
+}
+};
+
+function makeObjectAndAddToArray(name,price,description){
+    tempProduct={
+        name:name,
+        price:price,
+        description:description,
+    }
+    products.push(tempProduct);
+    console.log(products[0]);
+    tempProduct={};
+    reloadTable();
+};
+
+
+
+form.onsubmit=function(e){
+    e.preventDefault();
+    if(nameInput.value===""||priceInput.value===""||descriptionInput.value===""){
+        alert("dont miss any data !");
+    }
+    else{
+    makeObjectAndAddToArray(nameInput.value,priceInput.value,descriptionInput.value);
+    }
+    
+};
+
+function clearInputs(){
+    nameInput.value="";
+    priceInput.value="";
+    descriptionInput.value="";
+}
+
+cancleBtn.onclick=clearInputs;
+
+
+reloadTable();
